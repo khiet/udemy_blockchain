5785eedd7d90a09df2cd8cb59fc6cbb2c1f93cd4
Khiet Le <khiet.le@ehochef.com>
2020-03-08 16:50:20 +0000

    create blockchain

--- dev/test.js
+++ dev/test.js
@@ -1,23 +1,23 @@
 const Bockchain = require('./blockchain');
 
 const bitcoin = new Bockchain();
-bitcoin.createNewBlock(1234, 'dummyhash1', 'dummyhash2');
 bitcoin.createNewTransaction(100, 'khiet', 'nami');
-bitcoin.createNewBlock(1234, 'dummyhash1', 'dummyhash2');
-bitcoin.createNewTransaction(200, 'nami', 'keiko');
-bitcoin.createNewTransaction(300, 'khiet', 'keiko');
-bitcoin.createNewTransaction(400, 'hama', 'nami');
-bitcoin.createNewBlock(1234, 'dummyhash1', 'dummyhash2');
+bitcoin.createNewTransaction(200, 'khiet', 'keiko');
+
+// mining
+let nonce = bitcoin.proofOfWork(bitcoin.getLastBlock()['hash'], bitcoin.pendingTransactions);
+let hash = bitcoin.hashBlock(bitcoin.getLastBlock()['hash'], bitcoin.pendingTransactions, nonce)
+// add to blockchain
+bitcoin.createNewBlock(nonce, bitcoin.getLastBlock()['hash'], hash)
+
+bitcoin.createNewTransaction(500, 'yoko', 'nami');
+bitcoin.createNewTransaction(900, 'nami', 'keiko');
+
+// mining
+nonce = bitcoin.proofOfWork(bitcoin.getLastBlock()['hash'], bitcoin.pendingTransactions);
+hash = bitcoin.hashBlock(bitcoin.getLastBlock()['hash'], bitcoin.pendingTransactions, nonce)
+// add to blockchain
+bitcoin.createNewBlock(nonce, bitcoin.getLastBlock()['hash'], hash)
 
 console.log(bitcoin);
-console.log(bitcoin.getLastBlock()['transactions']);
-
-const hash = bitcoin.hashBlock('dummyhash1', bitcoin.getLastBlock()['transactions'], 1234);
-console.log(hash);
-
-const currentBlockData = bitcoin.getLastBlock()['transactions'];
-const nonce = bitcoin.proofOfWork('dummyhash1', currentBlockData);
-console.log(nonce);
-
-const powHash = bitcoin.hashBlock('dummyhash1', bitcoin.getLastBlock()['transactions'], nonce);
-console.log(powHash);
+console.log(bitcoin.getLastBlock().transactions);
